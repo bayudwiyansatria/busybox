a6458fd3f5fa7946cedbd31498e20ee37397f768
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2023-10-07 22:26:24 +0800

    Inject Version on Workflow call

--- etc/bin/release.sh
+++ etc/bin/release.sh
@@ -4,16 +4,19 @@
 DOCKER_REGISTRY=${1:-docker.io}
 REPOSITORY=${2:-bayudwiyansatria}
 TAG=${3:-latest}
+VERSION=${4:-}
 
 # Additional Parameter
-MODULES=${4:-}
+MODULES=${5:-}
 
 if [ -z "${MODULES}" ]; then
   docker build \
-    -t "${DOCKER_REGISTRY}"/"${REPOSITORY}":"${TAG}" \
-    .
+  --build-arg="VERSION=${VERSION}" \
+  -t "${DOCKER_REGISTRY}"/"${REPOSITORY}":"${TAG}" \
+  .
 else
   docker build \
+    --build-arg="VERSION=${VERSION}" \
     -t "${DOCKER_REGISTRY}"/"${REPOSITORY}":"${TAG}" \
     -f modules/"${MODULES}"/Dockerfile \
   .
